d69f308255eef60379711666885098c19483030f
Will Sherwood <willsherwood@gmail.com>
2022-09-01 20:49:01 -0700

    Made it so that no matter where project is located, file function works
    Previously had only my own file path

--- src/Blob.java
+++ src/Blob.java
@@ -1,17 +1,35 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.*;
 
 public class Blob {
 	
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        Blob fileHash = new Blob();
+        Blob fileHash = new Blob("/Users/willsherwood/eclipse-workspace/GitPrerequisites/test.txt");
         System.out.println(fileHash.sha1Code("test.txt"));
 	}
-
+	
+	public Blob (String filePath) throws NoSuchAlgorithmException, IOException {
+		generateFile(filePath, sha1Code(filePath));
+	}
+	
+	public void generateFile(String filePath, String sha1) throws IOException {
+		File source = new File(filePath);
+		File dest = new File(System.getProperty("user.dir")+ "/objects/" + sha1);
+		copyFileUsingStream(source, dest);
+	}
+	
 	public String sha1Code(String filePath) throws IOException, NoSuchAlgorithmException {
         FileInputStream fileInputStream = new FileInputStream(filePath);
         MessageDigest digest = MessageDigest.getInstance("SHA-1");
@@ -33,8 +51,25 @@ public class Blob {
 	            if (value < 16) {
 	                sb.append("0");
 	            }
-	            sb.append(Integer.toHexString(value).toUpperCase());
+	            sb.append(Integer.toHexString(value));
 	        }
 	        return sb.toString();
-	    }
+	 }
+	 
+	 private static void copyFileUsingStream(File source, File dest) throws IOException {
+		    InputStream is = null;
+		    OutputStream os = null;
+		    try {
+		        is = new FileInputStream(source);
+		        os = new FileOutputStream(dest);
+		        byte[] buffer = new byte[1024];
+		        int length;
+		        while ((length = is.read(buffer)) > 0) {
+		            os.write(buffer, 0, length);
+		        }
+		    } finally {
+		        is.close();
+		        os.close();
+		    }
+		}
 }
