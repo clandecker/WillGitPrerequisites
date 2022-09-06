29297c99c683b8f05b8e0ecf0a0dde9b8fa6856c
Will Sherwood <willsherwood@gmail.com>
2022-09-06 14:41:32 -0700

    Index now can add Blob & it now has a HashMap of fileName & SHA1

--- src/Index.java
+++ src/Index.java
@@ -1,16 +1,18 @@
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 public class Index {
-	private HashMap<Integer, Double> indexes;
+	private HashMap<String, String> hashMap;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 		Index myGit = new Index();
+		myGit.add("/Users/willsherwood/eclipse-workspace/GitPrerequisites/test.txt");
 	}
 	
 	public Index () throws IOException {
-		indexes = new HashMap<Integer, Double>();
+		hashMap = new HashMap<String, String>();
 		init();	
 	}
 	
@@ -20,14 +22,19 @@ public class Index {
 	}
 	
 	public void createFile() throws IOException {
-		System.out.println("Trying to create file");
 		File file = new File(System.getProperty("user.dir") + "/index.txt");
 		file.createNewFile();
 	}
 	
 	public void createDirectory() {
-		System.out.println("Trying to create folder");
 		new File(System.getProperty("user.dir")+ "/objects/").mkdirs();
 	}
 
+	public void add(String fileName) throws NoSuchAlgorithmException, IOException {
+		Blob toAdd = new Blob(fileName);
+		System.out.println(fileName);
+		System.out.println(toAdd.sha1Code(fileName));
+		hashMap.put(fileName, toAdd.sha1Code(fileName));
+		System.out.println(hashMap);
+	}
 }
