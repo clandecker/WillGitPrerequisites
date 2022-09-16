102e2f57b73e9c88ed3b45ce7482d51fff424644
benjiham <bham1@hwemail.com>
2022-09-15 23:23:36 -0700

    Tree Test
    Added the Tree test to j unit

--- src/Tree.java
+++ src/Tree.java
@@ -1,32 +1,33 @@
 import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.*;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Tree {
-	String input;
 	ArrayList <String> list;
 	String s;
 	
 	
-	public Tree() throws NoSuchAlgorithmException, IOException {
-		input = "";
+	public Tree(ArrayList<String> list) throws NoSuchAlgorithmException, IOException {
 		list = new ArrayList<String>();
-		s = "";
-		writeToFile(filename());
+		this.list = list;
+		s = filename();
+		writeToFile(s);
 	}
 	
 	
 	public String listToString() {
+		String temp = "";
 		for (int i = 0; i < list.size(); i++) {
-			s+= list.get(i) + "\n";
+			temp+= list.get(i) + "\n";
 		}
-		return s;
+		return temp;
 	}
 	
 	public String filename() throws NoSuchAlgorithmException, IOException {
-		return sha1Code(s);
+		return sha1Code(listToString());
 	}
 	
 	public String sha1Code(String fileName) throws IOException, NoSuchAlgorithmException {
@@ -55,7 +56,7 @@ public class Tree {
 	 }
 	 
 	 public void writeToFile (String filename) {
-			File file = new File("Test/index.txt");
+			File file = new File("objects/" + s);
 		    BufferedWriter bf = null;
 	        try {
 	            bf = new BufferedWriter(new FileWriter(file));
@@ -76,6 +77,26 @@ public class Tree {
 	            }
 	        }
 		}
+	 
+	 public static void main(String[]args) throws NoSuchAlgorithmException, IOException {
+			ArrayList <String> listy = new ArrayList<String>();
+			listy.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f");
+			listy.add("blob : 01d82591292494afd1602d175e165f94992f6f5f");
+			listy.add("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83");
+			listy.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+			listy.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
+			Tree tree = new Tree(listy);
+			File file = new File("objects/dd4840f48a74c1f97437b515101c66834b59b1be");
+			//assertTrue(file.exists());
+			Scanner s = new Scanner(file);
+//			assertTrue(s.nextLine().equals("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f"));
+//			assertTrue(s.nextLine().equals("blob : 01d82591292494afd1602d175e165f94992f6f5f"));
+//			assertTrue(s.nextLine().equals("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83"));
+//			assertTrue(s.nextLine().equals("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b"));
+//			assertTrue(s.nextLine().equals("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976"));
+			//file.delete();
+	 }
+	 
 	 
 	
 }
