e84299a2925af99b11cd4502a197035d853203a2
benjiham <bham1@hwemail.com>
2022-09-16 13:59:23 -0700

    fix test
    fix tree test

--- src/Tree.java
+++ src/Tree.java
@@ -87,14 +87,6 @@ public class Tree {
 			listy.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
 			Tree tree = new Tree(listy);
 			File file = new File("objects/dd4840f48a74c1f97437b515101c66834b59b1be");
-			//assertTrue(file.exists());
-			Scanner s = new Scanner(file);
-//			assertTrue(s.nextLine().equals("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f"));
-//			assertTrue(s.nextLine().equals("blob : 01d82591292494afd1602d175e165f94992f6f5f"));
-//			assertTrue(s.nextLine().equals("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83"));
-//			assertTrue(s.nextLine().equals("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b"));
-//			assertTrue(s.nextLine().equals("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976"));
-			//file.delete();
 	 }
 	 
 	 
